d6f8d5a528c35617ff9861da261372f73a2077f9
Rodrigo Cunha <66736583+rodrigodefcunha@users.noreply.github.com>
2021-06-22 11:01:03 -0300

    Atualização lista5

--- vkt_scraping_marca_modelo5_v0.py
+++ vkt_scraping_marca_modelo5_v0.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-marcas_modelos3 = pd.read_csv('datasets/marca_modelo_lista3_priority.csv')
-marcas_modelos4 = pd.read_csv('datasets/marca_modelo_lista4_priority.csv')
+df_marcas_modelos3 = pd.read_csv('datasets/marca_modelo_lista3_priority.csv')
+df_marcas_modelos4 = pd.read_csv('datasets/marca_modelo_lista4_priority.csv')
 
-marcas_modelos5 = pd.concat([marcas_modelos3, marcas_modelos4])
-marcas_modelos5 = marcas_modelos5.sort_values(['Consumo_Cidade_Gasolina'], ascending=False)
-marcas_modelos5.drop_duplicates(subset=['Chave_B'], inplace=True)
+df_marcas_modelos5 = pd.concat([df_marcas_modelos3, df_marcas_modelos4])
+df_marcas_modelos5 = df_marcas_modelos5.sort_values(['Consumo_Cidade_Gasolina'], ascending=False)
+df_marcas_modelos5.drop_duplicates(subset=['Chave_B'], inplace=True)
 
-marcas_modelos5 = marcas_modelos5[marcas_modelos5['Consumo_Cidade_Gasolina'].notnull()]
-marcas_modelos5 = marcas_modelos5.query("Consumo_Cidade_Gasolina != 'N/D' | Consumo_Cidade_Alcool != 'N/D'")
-marcas_modelos5.reset_index(drop=True, inplace=True)
-print(marcas_modelos5)
+df_marcas_modelos5 = df_marcas_modelos5[df_marcas_modelos5['Consumo_Cidade_Gasolina'].notnull()]
+df_marcas_modelos5 = df_marcas_modelos5.query("Consumo_Cidade_Gasolina != 'N/D' | Consumo_Cidade_Alcool != 'N/D'")
+df_marcas_modelos5['Modelo'] = df_marcas_modelos5['Modelo'].str.upper()
+df_marcas_modelos5.reset_index(drop=True, inplace=True)
+print(df_marcas_modelos5)
 
-marcas_modelos5.to_csv('datasets/marca_modelo_lista5_priority.csv', index=False)
+df_marcas_modelos5.to_csv('datasets/marca_modelo_lista5_priority.csv', index=False)
