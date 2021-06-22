7d9d6eaaee07ad91d6f70e17f575c56e715c6d7c
Rodrigo Cunha <66736583+rodrigodefcunha@users.noreply.github.com>
2021-06-22 11:35:40 -0300

    Atualização lista5

--- vkt_scraping_marca_modelo5_v0.py
+++ vkt_scraping_marca_modelo5_v0.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+def get_comb(comb):
+    flex = ['Álcool', 'Gasolina']
+    if(comb.isin(flex).all()):
+        return 'Flex'
+    elif(comb.isin(['Diesel']).any()):
+        return 'Diesel'
+    elif(comb.isin(['Gasolina']).any()):
+        return 'Gasolina'
+    elif(comb.isin(['Alcool']).any()):
+        return 'Alcool'
+    else:
+        return comb['Combustivel_Gasolina']
+
 df_marcas_modelos3 = pd.read_csv('datasets/marca_modelo_lista3_priority.csv')
 df_marcas_modelos4 = pd.read_csv('datasets/marca_modelo_lista4_priority.csv')
 
@@ -10,6 +23,7 @@ df_marcas_modelos5.drop_duplicates(subset=['Chave_B'], inplace=True)
 df_marcas_modelos5 = df_marcas_modelos5[df_marcas_modelos5['Consumo_Cidade_Gasolina'].notnull()]
 df_marcas_modelos5 = df_marcas_modelos5.query("Consumo_Cidade_Gasolina != 'N/D' | Consumo_Cidade_Alcool != 'N/D'")
 df_marcas_modelos5['Modelo'] = df_marcas_modelos5['Modelo'].str.upper()
+df_marcas_modelos5['Combustivel'] = df_marcas_modelos5[['Combustivel_Gasolina', 'Combustivel_Alcool']].apply(get_comb, axis=1)
 df_marcas_modelos5.reset_index(drop=True, inplace=True)
 print(df_marcas_modelos5)
 
