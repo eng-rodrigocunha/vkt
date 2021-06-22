3489fd34fcedafab6f0c82c5a150ba7de87aa6b7
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2021-06-22 17:52:57 -0300

    Atualiza vkt_calculo

--- vkt_calculo_preliminar_v0.py
+++ vkt_calculo_preliminar_v0.py
@@ -5,19 +5,19 @@ from unidecode import unidecode
 from difflib import get_close_matches
 
 def get_tipo (modelo):
-    motocicleta = ['CG', 'YS', 'YBS']
-    onibus = ['']
-    caminhao = ['']
+    motocicleta = ['CG', 'YS', 'YBS', 'FAZER']
+    #onibus = ['']
+    #caminhao = ['']
     #print(modelo)
     #print(type(modelo))
     #if(modelo is None):
     #    return ''
     if(any([x in modelo for x in motocicleta])):
         return 'MOTOCICLETA'
-    elif(any([x in modelo for x in onibus])):
-        return 'ONIBUS'
-    elif(any([x in modelo for x in caminhao])):
-        return 'CAMINHAO'
+    #elif(any([x in modelo for x in onibus])):
+    #    return 'ONIBUS'
+    #elif(any([x in modelo for x in caminhao])):
+    #    return 'CAMINHAO'
     else:
         return 'AUTOMOVEL'
 
@@ -30,6 +30,8 @@ def get_first_list (list):
 '''
 
 def get_vkt (df, ano_base):
+    #print(df)
+    #print(df.types)
     if(df['Tipo'] == 'AUTOMOVEL'):
         if(df['Combustivel'] == 'FLEX'):
             return vkt_flex(df['Ano'], ano_base)
@@ -94,17 +96,17 @@ def vkt_onibus (ano_fab, ano_base):
     else:
         return (-8.8551*(idade**3) + 263.41*(idade**2) - 4219.4*idade + 66435)
 
-def get_close_model (df_denatran, df_marca_modelo):
+def get_close_model (df_denatran):
+    #print(df_denatran)
     if(df_denatran['Tipo'] != 'AUTOMOVEL'):
         return ''
     
-    model = get_close_matches(df_denatran['Modelo_A'], df_marca_modelo, n=1)
+    model = get_close_matches(df_denatran.name, df_marca_modelo5['Modelo_A'], n=1)
 
     if(len(model) > 0):
         return model[0]
     else:
         return ''
-
 
 df_rms = pd.read_csv('datasets/rms.csv')
 
@@ -112,9 +114,10 @@ df_marca_modelo_denatran = pd.read_csv('datasets/2020/I_Frota_por_UF_Municipio_M
 df_marca_modelo_denatran.rename(columns={'Município': 'Municipio', 'Marca Modelo': 'Marca_Modelo', 'Ano Fabricação Veículo CRV': 'Ano', 'Qtd. Veículos': 'Quantidade'}, inplace = True)
 
 df_marca_modelo5 = pd.read_csv('datasets/marca_modelo_lista5_priority.csv')
+df_marca_modelo5.rename(columns={'Modelo': 'Modelo_A'}, inplace = True)
+df_marca_modelo5['Ano'] = df_marca_modelo5['Ano'].astype(np.int64)
 
 df_marca_modelo_denatran = df_marca_modelo_denatran[df_marca_modelo_denatran[['UF', 'Municipio']].apply(tuple, axis=1).isin(df_rms[['UF','Municipio']].apply(tuple, axis=1))]
-#df_marca_modelo_denatran[['RM', 'Capital']] = df_rms[['RM', 'Capital']]
 df_marca_modelo_denatran.reset_index(drop=True, inplace=True)
 df_marca_modelo_denatran[['Marca','Modelo']] = df_marca_modelo_denatran['Marca_Modelo'].str.split('/', n=1, expand=True)
 df_marca_modelo_denatran[['Modelo_A','Modelo_B']] = df_marca_modelo_denatran['Modelo'].str.split(' ', n=1, expand=True)
@@ -122,33 +125,40 @@ df_marca_modelo_denatran_null = df_marca_modelo_denatran[df_marca_modelo_denatra
 df_marca_modelo_denatran = df_marca_modelo_denatran[df_marca_modelo_denatran['Modelo_A'].notnull()]
 df_marca_modelo_denatran['Tipo'] = df_marca_modelo_denatran['Modelo_A'].map(lambda x: get_tipo(x))
 df_marca_modelo_denatran_modelos = df_marca_modelo_denatran.groupby(['Modelo_A']).sum().sort_values(by=['Quantidade'], ascending=False)
-print(df_marca_modelo_denatran_modelos)
-df_marca_modelo_denatran_modelos = df_marca_modelo_denatran[df_marca_modelo_denatran['Quantidade'] > np.percentile(df_marca_modelo_denatran['Quantidade'],95)]
-print(df_marca_modelo_denatran_modelos)
-df_marca_modelo_denatran_modelos['Modelo'] = df_marca_modelo_denatran_modelos.index.get_level_values('Modelo')
-df_marca_modelo_denatran_modelos['Tipo'] = df_marca_modelo_denatran_modelos.index.get_level_values('Modelo').map(lambda x: get_tipo(x))
-df_marca_modelo_denatran_modelos['Modelo_iCarros'] = df_marca_modelo_denatran_modelos[['Modelo_A', 'Tipo']].map(lambda x: get_close_model(x, df_marca_modelo5['Modelo']))
+#print(df_marca_modelo_denatran_modelos)
+df_marca_modelo_denatran_modelos = df_marca_modelo_denatran_modelos[df_marca_modelo_denatran_modelos['Quantidade'] > np.percentile(df_marca_modelo_denatran_modelos['Quantidade'],95)]
+#print(df_marca_modelo_denatran_modelos)
+#df_marca_modelo_denatran_modelos['Modelo_A'] = df_marca_modelo_denatran_modelos.index.get_level_values('Modelo_A')
+df_marca_modelo_denatran_modelos['Tipo'] = df_marca_modelo_denatran_modelos.index.get_level_values('Modelo_A').map(lambda x: get_tipo(x))
+df_marca_modelo_denatran_modelos['Modelo_iCarros'] = df_marca_modelo_denatran_modelos.apply(get_close_model, axis=1)
 #df_marca_modelo_denatran_modelos['Modelo_iCarros'] = df_marca_modelo_denatran_modelos['Modelo_iCarros'].map(lambda x: get_first_list(x))
-
-
-df_marca_modelo_denatran = pd.merge(df_marca_modelo_denatran, df_marca_modelo_denatran_modelos, on='Modelo_A')
 
 #df_marca_modelo_denatran['Modelo_iCarros'] = df_marca_modelo_denatran['Modelo_A'].map(lambda x: get_first_list(get_close_matches(x, df_marca_modelo5['Modelo'], n=1)))
 #df_marca_modelo_denatran['Modelo_iCarros'] = df_marca_modelo_denatran['Modelo_A'].map(lambda x: get_close_matches(x, df_marca_modelo5['Modelo'], n=1))
 #df_marca_modelo_denatran['Modelo_iCarros'] = df_marca_modelo_denatran['Modelo_iCarros'].map(lambda x: get_first_list(x))
 
-print('df_marca_modelo_denatran')
-print(df_marca_modelo_denatran)
+#print('df_marca_modelo_denatran')
+#print(df_marca_modelo_denatran)
 df_marca_modelo_denatran2 = df_marca_modelo_denatran[~df_marca_modelo_denatran['Ano'].map(lambda x: x.isnumeric())]
-print('df_marca_modelo_denatran2')
-print(df_marca_modelo_denatran2)
+#print('df_marca_modelo_denatran2')
+#print(df_marca_modelo_denatran2)
 df_marca_modelo_denatran = df_marca_modelo_denatran[df_marca_modelo_denatran['Ano'].map(lambda x: x.isnumeric())]
-print('df_marca_modelo_denatran')
-print(df_marca_modelo_denatran)
-
+#print('df_marca_modelo_denatran')
+#print(df_marca_modelo_denatran)
 df_marca_modelo_denatran['Ano'] = df_marca_modelo_denatran['Ano'].astype(np.int64)
-df_marca_modelo_denatran['Tipo'] = df_marca_modelo_denatran['Modelo_A'].map(lambda x: get_tipo(x))
+
+#print(df_marca_modelo_denatran.dtypes)
+#print(df_marca_modelo_denatran_modelos.dtypes)
+df_marca_modelo_denatran = pd.merge(df_marca_modelo_denatran, df_marca_modelo_denatran_modelos['Modelo_iCarros'], on='Modelo_A', how='outer')
+#print(df_marca_modelo_denatran)
+#print(df_marca_modelo5.dtypes)
+df_marca_modelo_denatran = pd.merge(df_marca_modelo_denatran, df_marca_modelo5[['Modelo_A', 'Ano', 'Combustivel', 'Consumo_Cidade_Gasolina', 'Consumo_Cidade_Alcool']], on=['Modelo_A', 'Ano'], how='outer')
+#print(df_rms.dtypes)
+df_marca_modelo_denatran = pd.merge(df_marca_modelo_denatran, df_rms[['Municipio', 'RM', 'Capital']], on='Municipio', how='outer')
+
+#df_marca_modelo_denatran['Tipo'] = df_marca_modelo_denatran['Modelo_A'].map(lambda x: get_tipo(x))
 #df_marca_modelo_denatran['VKT'] = df_marca_modelo_denatran[['Ano', 'Combustivel', 'Tipo']].apply(get_vkt, axis=1)
-df_marca_modelo_denatran['VKT'] = df_marca_modelo_denatran[['Ano', 'Combustivel', 'Tipo']].map(lambda x: get_vkt(x, 2020))
+df_marca_modelo_denatran['VKT'] = df_marca_modelo_denatran[['Ano', 'Combustivel', 'Tipo']].apply(lambda x: get_vkt(x, 2020), axis=1)
+df_marca_modelo_denatran['VKT'] = df_marca_modelo_denatran[['Ano', 'Combustivel', 'Tipo']].apply(lambda x: get_vkt(x, 2020), axis=1)
 
 df_marca_modelo_denatran.to_csv('datasets/2020/vkt_v0.csv', index=False)
